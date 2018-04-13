7c3552f3a438aac452772a95814db691171f980e
21287409 <21287409@student.uwl.ac.uk>
2018-04-13 12:43:04 +0100

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,136 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+include 'config.php';
+
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products || Online Shop</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/website.css" />
+  </head>
+  
+  <body>
+   <nav class="top-bar" data-topbar role="navigation">
+	<h1 class="logo">
+		<a href="index.php">Online Shop</a>
+	</h1>
+
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="dropdown menu main-navigation" data-dropdown-menu>
+          <li><a href="about.php">About</a></li>
+          <li class='active'>
+          	<a href="products.php">Products</a>
+          	<ul class="menu">
+			  <li><a href="laptops.php">Laptops</a></li>
+			  <li><a href="desktops.php">Desktops</a></li>
+			  <li><a href="phones.php">Phones</a></li>
+			  <li><a href="Headphones.php">Headphones</a></li>
+			</ul>
+          </li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+    <div class="row" style="margin-top:10px;">
+      <div class="large-12">
+        <?php
+
+          echo '<p><h3>Your Shopping Cart</h3></p>';
+
+          if(isset($_SESSION['cart'])) {
+
+            $total = 0;
+            echo '<table>';
+            echo '<tr>';
+            echo '<th>Code</th>';
+            echo '<th>Name</th>';
+            echo '<th>Quantity</th>';
+            echo '<th>Cost</th>';
+            echo '</tr>';
+            foreach($_SESSION['cart'] as $product_id => $quantity) {
+
+            $result = $mysqli->query("SELECT product_code, product_name, product_desc, qty, price FROM products WHERE id = ".$product_id);
+
+
+            if($result){
+
+              while($obj = $result->fetch_object()) {
+                $cost = $obj->price * $quantity; //work out the line cost
+                $total = $total + $cost; //add to the total cost
+
+                echo '<tr>';
+                echo '<td>'.$obj->product_code.'</td>';
+                echo '<td>'.$obj->product_name.'</td>';
+                echo '<td>'.$quantity.'&nbsp;<a class="button [secondary success alert]" style="padding:5px;" href="update-cart.php?action=add&id='.$product_id.'">+</a>&nbsp;<a class="button alert" style="padding:5px;" href="update-cart.php?action=remove&id='.$product_id.'">-</a></td>';
+                echo '<td>£ '.$cost.'</td>';
+                echo '</tr>';
+              }
+            }
+
+          }
+
+          echo '<tr>';
+          echo '<td colspan="3" align="right">Total</td>';
+          echo '<td>£ '.$total.'</td>';
+          echo '</tr>';
+
+          echo '<tr>';
+          echo '<td colspan="4" align="right"><a href="update-cart.php?action=empty" class="button alert">Empty Cart</a>&nbsp;<a href="products.php" class="button [secondary success alert]">Continue Shopping</a>&nbsp;<a href="orders-update.php" class="button" [secondary success alert]">BUY</a>';
+      
+          echo '</td>';
+
+          echo '</tr>';
+          echo '</table>';
+        }
+
+        else {
+          echo "You have no items in your shopping cart.";
+        }
+
+          echo '</div>';
+          echo '</div>';
+          ?>
+
+    <div class="row" style="margin-top:10px;">
+      <div class="small-12">
+
+
+        <footer class="main-footer">
+           <span>&copy;2018 - Online Shop. All Rights Reserved.</span>
+        </footer>
+    </div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ phones.php
@@ -0,0 +1,116 @@
+<?php
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+include 'config.php';
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products || Online Shop</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/website.css" />
+  </head>
+  
+  <body>
+   <nav class="top-bar" data-topbar role="navigation">
+	<h1 class="logo">
+		<a href="index.php">Online Shop</a>
+	</h1>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="dropdown menu main-navigation" data-dropdown-menu>
+          <li><a href="about.php">About</a></li>
+          <li class='active'>
+          	<a href="products.php">Products</a>
+          	<ul class="menu">
+			  <li><a href="laptops.php">Laptops</a></li>
+			  <li><a href="desktops.php">Desktops</a></li>
+			  <li><a href="phones.php">Phones</a></li>
+			  <li><a href="Headphones.php">Headphones</a></li>
+			</ul>
+          </li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+    
+	<div class="grid-container">
+   
+
+    <div class="grid-x grid-padding-x">
+        <?php
+          $i=0;
+          $product_id = array();
+          $product_quantity = array();
+
+          $result = $mysqli->query('SELECT * FROM products WHERE id IN (11, 12, 13, 14, 15)');
+          if($result === FALSE){
+            die(mysql_error());
+          }
+
+          if($result){
+
+            while($obj = $result->fetch_object()) {
+
+              echo '<div class="large-4 cell item-for-sale">';
+              echo '<h3>'.$obj->product_name.'</h3>';
+              echo '<img src="images/products/'.$obj->product_img_name.'"/>';
+				echo '<ul class="tech-features">';
+              echo '<li class="device-features"><span>Product Code</span>'.$obj->product_code.'</li>';
+              echo '<li class="device-features"><span>Description</span>'.$obj->product_desc.'</li>';
+              echo '<li class="device-features"><span>Units Available</span>'.$obj->qty.'</li>';
+              echo '<li class="device-features"><span>Price (Per Unit)</span>'.$currency.$obj->price.'</li>';
+
+              if($obj->qty > 0){
+                echo '<li class="buy-this"><a href="update-cart.php?action=add&id='.$obj->id.'"><input type="submit" value="Add To Cart"  /></a></li>';
+              }
+              else {
+                echo '<li class="out-of-stock">Out Of Stock!</li>';
+              }
+              echo '</div>';
+
+              $i++;
+            }
+
+          }
+
+          $_SESSION['product_id'] = $product_id;
+
+          echo '</div>';
+          echo '</div>';
+          ?>
+
+        <div class="row" style="margin-top:10px;">
+          <div class="small-12">
+
+        <footer class="main-footer">
+           <span>&copy;2018 - Online Shop. All Rights Reserved.</span>
+        </footer>
+    </div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+		</div>
+  </body>
+</html>
